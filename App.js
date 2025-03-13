2b69b1beac51bf4083e3ba94cc55d1337f26978c
VeetiT <n3tive00@students.oamk.fi>
2025-03-13 20:07:04 +0200

    Added basic navigation

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
+import { Button, IconButton, Text } from 'react-native-paper';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name={'Locations'} component={Locations}/>
+        <Stack.Screen name={'Add location'} component={AddLocation}/>
+        <Stack.Screen name={'Map view'} component={MapView}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+}
+
+function Locations(){
+
+  const navigation = useNavigation();  
+
+  return(
+    <Pressable onPress={() => navigation.navigate('Add location')}>
+      <Text variant='displayMedium'>Locations page</Text>
+    </Pressable>
+  );
+}
+
+function AddLocation(){
+  const navigation = useNavigation(); 
+
+  return(
+    <View>
+      <IconButton icon={'home'} size={52} onPress={()=> navigation.navigate('Locations')}/>
+        <Button onPress={() => navigation.navigate('Map view')}> Map view</Button>
+    </View>
+  )
+}
+
+function MapView(){
+  return(<Text variant='displayMedium'>Map view page</Text>)
 }
 
 const styles = StyleSheet.create({
